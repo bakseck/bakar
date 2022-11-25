fc9f41b6129112bd94d60ee4d7ea787a0a54026c
bakseck <seck.bakar@ugb.edu.sn>
2022-11-25 22:33:41 +0000

    My own package

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,4 +1,20 @@
 package bakar 
 
 func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error))  {
+	b := BTreeLevelCount(root)
+	for i := 1; i <= b; i++ {
+		BtreeCurrentLevel(root, i, f)
+	}
+}
+
+func BtreeCurrentLevel()  {
+	if root == nil {
+		return
+	}
+	if b == 1 {
+		f(root.Data)
+	} else if b > 1 {
+		BtreeCurrentLevel(root.Left, b-1, f)
+		BtreeCurrentLevel(root.Right, b-1, f)
+	}
 }
